eec1a970baa83fa421165b2a7e61639a321977b2
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-08-01 01:00:33 -0500

    Cambio de tipo de database

---
+++ database/control_gastos.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 01, 2025 at 03:37 AM
+-- Server version: 8.0.42-0ubuntu0.24.04.2
+-- PHP Version: 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `control_gastos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gastos`
+--
+
+CREATE TABLE `gastos` (
+  `id` int NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `valor` int NOT NULL,
+  `id_categoria` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gasto_categoria`
+--
+
+CREATE TABLE `gasto_categoria` (
+  `id` int NOT NULL,
+  `id_gasto` int NOT NULL,
+  `id_categoria` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL,
+  `primer_nombre` varchar(255) NOT NULL,
+  `segundo_nombre` varchar(255) DEFAULT NULL,
+  `primer_apellido` varchar(255) NOT NULL,
+  `segundo_apellido` varchar(255) DEFAULT NULL,
+  `correo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario_gasto_categoria`
+--
+
+CREATE TABLE `usuario_gasto_categoria` (
+  `id` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `id_gasto_categoria` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gastos`
+--
+ALTER TABLE `gastos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Indexes for table `gasto_categoria`
+--
+ALTER TABLE `gasto_categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_gasto` (`id_gasto`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuario_gasto_categoria`
+--
+ALTER TABLE `usuario_gasto_categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_gasto_categoria` (`id_gasto_categoria`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gastos`
+--
+ALTER TABLE `gastos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gasto_categoria`
+--
+ALTER TABLE `gasto_categoria`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuario_gasto_categoria`
+--
+ALTER TABLE `usuario_gasto_categoria`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `gastos`
+--
+ALTER TABLE `gastos`
+  ADD CONSTRAINT `gastos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`);
+
+--
+-- Constraints for table `gasto_categoria`
+--
+ALTER TABLE `gasto_categoria`
+  ADD CONSTRAINT `gasto_categoria_ibfk_1` FOREIGN KEY (`id_gasto`) REFERENCES `gastos` (`id`),
+  ADD CONSTRAINT `gasto_categoria_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`);
+
+--
+-- Constraints for table `usuario_gasto_categoria`
+--
+ALTER TABLE `usuario_gasto_categoria`
+  ADD CONSTRAINT `usuario_gasto_categoria_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `usuario_gasto_categoria_ibfk_2` FOREIGN KEY (`id_gasto_categoria`) REFERENCES `gasto_categoria` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
